269e7b210ac0791fdff3c426b766b2f1f78f0241
DumebiIjomah <140549885+DumebiIjomah@users.noreply.github.com>
2024-10-26 10:48:01 +0530

    Milestone 2 Commit
    Milestone 2 Commit

---
+++ src/server/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import httpErrors from 'http-errors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import * as path from 'path';
+import dotenv from 'dotenv';
+import { timeMiddleware } from "./middleware/time";
+
+
+import rootRoutes from './routes/root';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(timeMiddleware);
+app.use(
+  express.static(path.join(process.cwd(), "src", "public"))
+ );
+app.use(morgan('dev'));                
+app.use(express.json());                 
+app.use(express.urlencoded({ extended: false })); 
+app.use(cookieParser());                  
+
+
+app.use(express.static(path.join(process.cwd(), 'src', 'public')));
+
+app.set('views', path.join(process.cwd(), 'src', 'server', 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRoutes);
+
+app.use((_request, _response, next) => {
+  next(httpErrors(404));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
