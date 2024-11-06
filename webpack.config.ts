d2b34cfa7cf5b32ec0900dee43be08d59b71a9e3
Fasika Abera <contact.fasikaabera@gmail.com>
2024-11-05 19:45:14 -0800

    Set up the skeleton and tooling for the front-end code

---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import path from "path";
+import webpack from "webpack";
+
+dotenv.config();
+
+const mode =
+	process.env.NODE_ENV === "production" ?
+		"production" : "development";
+
+const config: webpack.Configuration = {
+	entry: {
+		main: "./src/client/main.ts",
+		// main: path.join(process.cwd(), "src", "client", "main.ts"),
+	},
+	mode,
+	output: {
+		path: path.join(process.cwd(), "src", "public", "js"),
+		filename: "[name].js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+};
+export default config;
